58a01572c4b428cd8881978b8db93c833baed9a7
Gustav Lillesæter <gustavli@users.noreply.github.com>
2021-09-03 15:18:58 +0200

    Digihot 602 sitteputevalg (#20)
    * endre model for sitteputevalg
    
    
    * gamle sitteputeenumer for bakoverkompatibilitet

--- src/main/kotlin/no/nav/hjelpemidler/soknad/db/domain/SøknadForBruker.kt
+++ src/main/kotlin/no/nav/hjelpemidler/soknad/db/domain/SøknadForBruker.kt
@@ -248,11 +248,12 @@ private fun rullestolinfo(hjelpemiddel: JsonNode): RullestolInfo? {
     return RullestolInfo(
         skalBrukesIBil = rullestolInfoJson["skalBrukesIBil"]?.booleanValue(),
         sitteputeValg = when (rullestolInfoJson["sitteputeValg"]?.textValue()) {
+            "TrengerSittepute" -> SitteputeValg.TrengerSittepute
             "StandardSittepute" -> SitteputeValg.StandardSittepute
             "LeggesTilSeparat" -> SitteputeValg.LeggesTilSeparat
             "HarFraFor" -> SitteputeValg.HarFraFor
             null -> null
-            else -> throw RuntimeException("Ugyldig signaturtype")
+            else -> throw RuntimeException("Ugyldig sitteputeValg")
         }
     )
 }
@@ -351,7 +352,7 @@ data class RullestolInfo(
 )
 
 enum class SitteputeValg {
-    StandardSittepute, LeggesTilSeparat, HarFraFor
+    TrengerSittepute, HarFraFor, StandardSittepute, LeggesTilSeparat
 }
 
 data class UtlevertInfo(
