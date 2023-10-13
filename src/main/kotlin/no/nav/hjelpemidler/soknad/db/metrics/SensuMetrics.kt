e2dcb840c5328ff3f49c36c4e53d118570330f60
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2023-10-13 12:16:46 +0200

    fjern siste rest av sensu

--- src/main/kotlin/no/nav/hjelpemidler/soknad/db/metrics/SensuMetrics.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.hjelpemidler.soknad.db.metrics
-
-import mu.KotlinLogging
-
-private val logg = KotlinLogging.logger {}
-// private val sikkerlogg = KotlinLogging.logger("tjenestekall")
