748db59fe3782e73201dc18799a95400cd65d1a1
Erik Eiesland <erik.eiesland@nav.no>
2024-07-29 15:07:21 +0200

    oppdaterer status under lagring av vedtaksresultat

--- app/src/main/kotlin/no/nav/hjelpemidler/soknad/db/soknad/SøknadService.kt
+++ app/src/main/kotlin/no/nav/hjelpemidler/soknad/db/soknad/SøknadService.kt
@@ -1,6 +1,7 @@
 package no.nav.hjelpemidler.soknad.db.soknad
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import no.nav.hjelpemidler.behovsmeldingsmodell.BehovsmeldingStatus
 import no.nav.hjelpemidler.behovsmeldingsmodell.SøknadId
 import no.nav.hjelpemidler.behovsmeldingsmodell.sak.Fagsak
 import no.nav.hjelpemidler.behovsmeldingsmodell.sak.Sakstilknytning
@@ -21,16 +22,14 @@ class SøknadService(private val transaction: Transaction) {
         sakstilknytning: Sakstilknytning,
     ): Int {
         logg.info { "Knytter sakId: ${sakstilknytning.sakId} til søknadId: $søknadId, system: ${sakstilknytning.system}" }
-        return when (sakstilknytning) {
-            is Sakstilknytning.Hotsak -> transaction {
-                hotsakStore.lagKnytningMellomSakOgSøknad(
+        return transaction {
+            when (sakstilknytning) {
+                is Sakstilknytning.Hotsak -> hotsakStore.lagKnytningMellomSakOgSøknad(
                     søknadId = søknadId,
                     sakId = sakstilknytning.sakId,
                 )
-            }
 
-            is Sakstilknytning.Infotrygd -> transaction {
-                infotrygdStore.lagKnytningMellomSakOgSøknad(
+                is Sakstilknytning.Infotrygd -> infotrygdStore.lagKnytningMellomSakOgSøknad(
                     søknadId = søknadId,
                     sakId = sakstilknytning.sakId,
                     fnrBruker = sakstilknytning.fnrBruker,
@@ -43,18 +42,20 @@ class SøknadService(private val transaction: Transaction) {
         søknadId: SøknadId,
         vedtaksresultat: Vedtaksresultat,
     ): Int {
-        logg.info { "Lagrer vedtaksresultat for søknadId: $søknadId, system: ${vedtaksresultat.system}" }
-        return when (vedtaksresultat) {
-            is Vedtaksresultat.Hotsak -> transaction {
-                hotsakStore.lagreVedtaksresultat(
+        val nyStatus = BehovsmeldingStatus.fraVedtaksresultat(vedtaksresultat)
+        logg.info { "Lagrer vedtaksresultat for søknadId: $søknadId, system: ${vedtaksresultat.system}, nyStatus: $nyStatus" }
+        return transaction {
+            if (søknadStore.oppdaterStatus(søknadId, nyStatus) == 0) {
+                logg.warn { "Status for søknadId: $søknadId er allerede $nyStatus" }
+            }
+            when (vedtaksresultat) {
+                is Vedtaksresultat.Hotsak -> hotsakStore.lagreVedtaksresultat(
                     søknadId = søknadId,
                     vedtaksresultat = vedtaksresultat.vedtaksresultat,
                     vedtaksdato = vedtaksresultat.vedtaksdato,
                 )
-            }
 
-            is Vedtaksresultat.Infotrygd -> transaction {
-                infotrygdStore.lagreVedtaksresultat(
+                is Vedtaksresultat.Infotrygd -> infotrygdStore.lagreVedtaksresultat(
                     søknadId = søknadId,
                     vedtaksresultat = vedtaksresultat.vedtaksresultat,
                     vedtaksdato = vedtaksresultat.vedtaksdato,
