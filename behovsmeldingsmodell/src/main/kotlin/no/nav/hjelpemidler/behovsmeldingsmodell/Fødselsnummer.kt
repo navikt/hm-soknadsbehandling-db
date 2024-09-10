1b9d36ce400c94ed22085caabda66a51b47390fe
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2024-09-10 14:16:17 +0200

    Grunnleggende validering på fødselsnummer

--- behovsmeldingsmodell/src/main/kotlin/no/nav/hjelpemidler/behovsmeldingsmodell/Fødselsnummer.kt
+++ behovsmeldingsmodell/src/main/kotlin/no/nav/hjelpemidler/behovsmeldingsmodell/Fødselsnummer.kt
@@ -3,6 +3,11 @@ package no.nav.hjelpemidler.behovsmeldingsmodell
 import com.fasterxml.jackson.annotation.JsonValue
 
 data class Fødselsnummer(private val value: String) {
+
+    init {
+        require(value.all { it.isDigit() } && value.length == 11) { "$value er ikke et gyldig fødselsnummer. Må bestå av 11 siffer." }
+    }
+
     @JsonValue
     override fun toString(): String = value
 }
