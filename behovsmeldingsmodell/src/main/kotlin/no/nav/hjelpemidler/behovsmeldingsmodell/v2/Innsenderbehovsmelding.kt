e2cd669d6a235ed9fe5df8729333e05b0418bb1c
Erik Eiesland <erik.eiesland@nav.no>
2025-01-22 08:08:57 +0100

    hotlibs/katalog -> 25.021.132849

--- behovsmeldingsmodell/src/main/kotlin/no/nav/hjelpemidler/behovsmeldingsmodell/v2/Innsenderbehovsmelding.kt
+++ behovsmeldingsmodell/src/main/kotlin/no/nav/hjelpemidler/behovsmeldingsmodell/v2/Innsenderbehovsmelding.kt
@@ -168,7 +168,7 @@ data class Tilbehør(
     val antall: Int,
     val begrunnelse: String?,
     val fritakFraBegrunnelseÅrsak: FritakFraBegrunnelseÅrsak?,
-    val opplysninger: List<Opplysning> = emptyList()
+    val opplysninger: List<Opplysning> = emptyList(),
 )
 
 data class Utlevertinfo(
