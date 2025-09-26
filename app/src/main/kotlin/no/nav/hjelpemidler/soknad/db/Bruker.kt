4d1a029c5319ee726f7bbf5d5625364b873c1042
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2025-09-26 10:15:07 +0200

    Fiks copy/paste feil i hierarkiet

--- app/src/main/kotlin/no/nav/hjelpemidler/soknad/db/Bruker.kt
+++ app/src/main/kotlin/no/nav/hjelpemidler/soknad/db/Bruker.kt
@@ -1,12 +1,11 @@
 package no.nav.hjelpemidler.soknad.db
 
 import io.ktor.resources.Resource
-import no.nav.hjelpemidler.soknad.db.soknad.Søknader
 
 @Resource("/bruker")
 class Bruker {
     @Resource("/dokumenter")
-    class Dokumenter(val parent: Søknader = Søknader()) {
+    class Dokumenter(val parent: Bruker = Bruker()) {
         @Resource("/{fagsakId}")
         class ForSak(val fagsakId: String, val parent: Dokumenter = Dokumenter())
     }
