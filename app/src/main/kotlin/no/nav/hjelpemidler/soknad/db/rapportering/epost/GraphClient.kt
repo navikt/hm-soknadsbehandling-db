0608d3792f7a430c0f345025d41d0d1fa5e50d91
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2025-11-27 12:26:55 +0100

    Skru på logging av epostrequest for feilsøking

--- app/src/main/kotlin/no/nav/hjelpemidler/soknad/db/rapportering/epost/GraphClient.kt
+++ app/src/main/kotlin/no/nav/hjelpemidler/soknad/db/rapportering/epost/GraphClient.kt
@@ -3,8 +3,14 @@ package no.nav.hjelpemidler.soknad.db.rapportering.epost
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.accept
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import no.nav.hjelpemidler.http.createHttpClient
@@ -15,7 +21,19 @@ private val log = KotlinLogging.logger { }
 
 class GraphClient(
     private val openIDClient: OpenIDClient,
-    private val httpClient: HttpClient = createHttpClient(Apache.create()) { expectSuccess = true },
+    private val httpClient: HttpClient = createHttpClient(Apache.create()) {
+        expectSuccess = true
+
+        // TODO fjern logging
+        install(Logging) {
+            level = LogLevel.BODY
+        }
+
+        defaultRequest {
+            accept(ContentType.Application.Json)
+            contentType(ContentType.Application.Json)
+        }
+    },
     private val baseUrl: String = "https://graph.microsoft.com/v1.0",
     private val scope: String = "https://graph.microsoft.com/.default",
 ) {
