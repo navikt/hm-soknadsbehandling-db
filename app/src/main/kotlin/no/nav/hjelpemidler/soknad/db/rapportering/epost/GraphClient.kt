69a41eb9d41fc17668474ef02c21be84fadb55e0
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2025-11-27 13:06:27 +0100

    Fjern logging av epostrequest

--- app/src/main/kotlin/no/nav/hjelpemidler/soknad/db/rapportering/epost/GraphClient.kt
+++ app/src/main/kotlin/no/nav/hjelpemidler/soknad/db/rapportering/epost/GraphClient.kt
@@ -3,14 +3,8 @@ package no.nav.hjelpemidler.soknad.db.rapportering.epost
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
-import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.request.accept
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
-import io.ktor.http.ContentType
-import io.ktor.http.contentType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import no.nav.hjelpemidler.http.createHttpClient
@@ -23,32 +17,22 @@ class GraphClient(
     private val openIDClient: OpenIDClient,
     private val httpClient: HttpClient = createHttpClient(Apache.create()) {
         expectSuccess = true
-
-        // TODO fjern logging
-        install(Logging) {
-            level = LogLevel.BODY
-        }
-
-        defaultRequest {
-            accept(ContentType.Application.Json)
-            contentType(ContentType.Application.Json)
-        }
     },
     private val baseUrl: String = "https://graph.microsoft.com/v1.0",
     private val scope: String = "https://graph.microsoft.com/.default",
 ) {
 
-    suspend fun sendEpost(request: SendMailRequest, avsender: String) {
+    suspend fun sendEpost(epostRequest: SendMailRequest, avsender: String) {
         try {
             withContext(Dispatchers.IO) {
                 val tokenSet = openIDClient.grant(scope)
                 httpClient.post("$baseUrl/users/$avsender/sendMail") {
                     bearerAuth(tokenSet)
-                    setBody(request)
+                    setBody(epostRequest)
                 }
             }
         } catch (t: Throwable) {
-            log.error(t) { "Sending av epost feilet for $request, avsender=$avsender" }
+            log.error(t) { "Sending av epost feilet for $epostRequest, avsender=$avsender" }
             throw t
         }
     }
