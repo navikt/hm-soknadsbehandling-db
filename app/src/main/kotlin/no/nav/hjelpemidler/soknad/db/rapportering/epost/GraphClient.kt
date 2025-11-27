f497e79f826572f1569ac8e0f729c7b238b0b5ee
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2025-11-27 13:18:32 +0100

    Epostrequest må ha json ContentType

--- app/src/main/kotlin/no/nav/hjelpemidler/soknad/db/rapportering/epost/GraphClient.kt
+++ app/src/main/kotlin/no/nav/hjelpemidler/soknad/db/rapportering/epost/GraphClient.kt
@@ -3,8 +3,12 @@ package no.nav.hjelpemidler.soknad.db.rapportering.epost
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.accept
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import no.nav.hjelpemidler.http.createHttpClient
@@ -17,6 +21,10 @@ class GraphClient(
     private val openIDClient: OpenIDClient,
     private val httpClient: HttpClient = createHttpClient(Apache.create()) {
         expectSuccess = true
+        defaultRequest {
+            accept(ContentType.Application.Json)
+            contentType(ContentType.Application.Json)
+        }
     },
     private val baseUrl: String = "https://graph.microsoft.com/v1.0",
     private val scope: String = "https://graph.microsoft.com/.default",
